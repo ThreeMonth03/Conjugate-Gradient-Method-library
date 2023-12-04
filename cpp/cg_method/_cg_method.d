e3e65a962daa205ecaecb1bf277f07078951f3a8
threemonth03 <austin20463@gmail.com>
2023-12-04 11:51:55 +0000

    Remove objects and dependencys.

--- cpp/cg_method/_cg_method.d
+++
@@ -1,7 +0,0 @@
-cpp/cg_method/_cg_method.o: cpp/cg_method/_cg_method.cpp \
- /workspace/Conjugate-Gradient-Method-library/cpp/cg_method/_cg_method.hpp \
- /workspace/Conjugate-Gradient-Method-library/cpp/matrix/_matrix.hpp
-
-/workspace/Conjugate-Gradient-Method-library/cpp/cg_method/_cg_method.hpp:
-
-/workspace/Conjugate-Gradient-Method-library/cpp/matrix/_matrix.hpp:
--- cpp/matrix/_matrix.d
+++
@@ -1,4 +0,0 @@
-cpp/matrix/_matrix.o: cpp/matrix/_matrix.cpp \
- /workspace/Conjugate-Gradient-Method-library/cpp/matrix/_matrix.hpp
-
-/workspace/Conjugate-Gradient-Method-library/cpp/matrix/_matrix.hpp:
